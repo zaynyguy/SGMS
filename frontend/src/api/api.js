04119a628ec959ba073f9c082376311603ef5e36
Akrem Mohammed <pcdellexecute.gmail.com>
2025-08-22 17:30:11 +0300

    anything again

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.DATABASE_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = async (endpoint, method = 'GET', body = null) => {
   const config = {
