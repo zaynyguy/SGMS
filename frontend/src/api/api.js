10f0838f420689d66973ee07e3e1656ec94e3bc0
Akrem Mohammed <pcdellexecute.gmail.com>
2025-08-22 17:23:37 +0300

    some changes to enhance the ui

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.DATABASE_URL;
 
 export const api = async (endpoint, method = 'GET', body = null) => {
   const config = {
